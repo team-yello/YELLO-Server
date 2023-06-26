467e7514f7eadd7bc9eb4d42bb5ed7a9e07cbb38
Kwon Sehoon <devkwonsehoon@naver.com>
2023-06-26 19:51:52 +0900

    YEL-9 [setting] CI/CD 적용

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd /home/ec2-user/app
+DOCKER_APP_NAME=spring
+
+# BLUE 서버 체크
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+echo "[배포 시작] $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+
+# GREEN이 실행중이면 BLUE up
+if [ -z "$EXIST_BLUE" ]; then
+  echo "[BLUE] 배포가 시작됩니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+  sleep 30
+  BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+  if [ -z "$BLUE_HEALTH" ]; then
+    sudo ./slack_blue.sh
+  else
+    echo "[GREEN] 서버를 종료합니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    sudo docker image prune -af
+    echo "[GREEN] 서버를 종료했습니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+  fi
+
+# BLUE가 실행중이면 GREEN up
+else
+	echo "[GREEN] 배포가 시작됩니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+  sleep 30
+
+  GREEN_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
+
+  if [ -z "$GREEN_HEALTH" ]; then
+      sudo ./slack_green.sh
+  else
+    echo "[BLUE] 서버를 종료합니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+    sudo docker image prune -af
+    echo "[BLUE] 서버를 종료했습니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+  fi
+fi
+  echo "[배포 종료] $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+
+  echo "===================== 배포 완료 =====================" >> /home/ec2-user/deploy.log
+  echo >> home/ec2-user/deploy.log
