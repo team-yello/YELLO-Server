d833b7f094fb66910987c43eeb99b28cc517e145
Kwon Sehoon <devkwonsehoon@naver.com>
2023-06-26 20:53:48 +0900

    YEL-9 [setting] 프로젝트 재정의

--- src/main/java/com/yello/server/entity/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yello.server.entity.user;
-
-import com.yello.server.entity.user.User;
-import org.springframework.data.repository.Repository;
-
-public interface UserRepository extends Repository<User, Long> {
-    // CREATE
-}
