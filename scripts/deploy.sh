3ad5491c7beed622719094830cf4780e8a752184
Kwon Sehoon <devkwonsehoon@naver.com>
2023-06-27 21:39:23 +0900

    YEL-9 [setting] deploy.sh 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,4 +42,4 @@ fi
   echo "[배포 종료] $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
 
   echo "===================== 배포 완료 =====================" >> /home/ec2-user/deploy.log
-  echo >> home/ec2-user/deploy.log
+  echo >> /home/ec2-user/deploy.log
