115f5358dc18ac282bf569724f666fb807f8bf09
Kwon Sehoon <devkwonsehoon@naver.com>
2023-06-28 14:52:40 +0900

    YEL-9 [setting] port testing

--- src/main/java/com/yello/server/ServerApplication.java
+++ src/main/java/com/yello/server/ServerApplication.java
@@ -1,12 +1,17 @@
 package com.yello.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
 
+    @Value("${server.port}")
+    private static int port;
+
     public static void main(String[] args) {
+        System.out.println(port);
         SpringApplication.run(ServerApplication.class, args);
     }
 
