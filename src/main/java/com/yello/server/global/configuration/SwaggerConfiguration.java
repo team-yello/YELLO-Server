4ec1ffc2f0df4457d453db096457415aca760a24
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-03 23:50:52 +0900

    YEL-17 [chore] Swagger 설정 업데이트

--- src/main/java/com/yello/server/global/configuration/SwaggerConfiguration.java
+++ src/main/java/com/yello/server/global/configuration/SwaggerConfiguration.java
@@ -2,26 +2,27 @@ package com.yello.server.global.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @OpenAPIDefinition(
-        info = @Info(title = "YELL:O API Documentation",
-                description = "YELL:O API 공식 명세서 입니다.",
-                version = "v1"))
+    info = @Info(title = "YELL:O API Documentation",
+        description = "YELL:O API 공식 명세서 입니다.",
+        version = "v1",
+        license = @License(
+            name = "Apache License Version 2.0",
+            url = "http://www.apache.org/licenses/LICENSE-2.0"
+        )
+    ),
+    tags = {
+        @Tag(name = "01. Vote", description = "투표(Yello) API"),
+    }
+)
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfiguration {
-    @Bean
-    public GroupedOpenApi openApi() {
-        String[] paths = {"api/v1/**"};
 
-        return GroupedOpenApi.builder()
-                .group("V1 API")
-                .pathsToMatch(paths)
-                .build();
-    }
 }
