62f98f759f8d2015acfc7113c56b79e3a8df2c06
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-03 18:36:42 +0900

    YEL-17 [chore] 작성일자 포맷팅을 위한 유틸 클래스 선언

---
+++ src/main/java/com/yello/server/global/common/util/TimeUtil.java
@@ -0,0 +1,27 @@
+package com.yello.server.global.common.util;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TimeUtil {
+
+    private static final int SECOND = 60;
+    private static final int MINUTE = 60;
+    private static final int HOUR = 24;
+
+    public static String toFormattedString(LocalDateTime localDateTime) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        Duration duration = Duration.between(localDateTime, currentDateTime);
+        long seconds = duration.getSeconds();
+
+        if (seconds < SECOND) {
+            return seconds + "초 전";
+        } else if ((seconds /= SECOND) < MINUTE) {
+            return seconds + "분 전";
+        } else if ((seconds /= MINUTE) < HOUR) {
+            return (seconds) + "시간 전";
+        }
+
+        return (seconds) + "일 전";
+    }
+}
