46efc4a5e30341b7f33cb07356c5945dee9f5c4b
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-04 00:40:02 +0900

    YEL-22 [feat] 투표 상세 정보 조회 API 구현

--- src/main/java/com/yello/server/domain/vote/service/VoteServiceImpl.java
+++ src/main/java/com/yello/server/domain/vote/service/VoteServiceImpl.java
@@ -1,9 +1,13 @@
 package com.yello.server.domain.vote.service;
 
+import static com.yello.server.global.common.ErrorCode.NOT_FOUND_VOTE_EXCEPTION;
+
 import com.yello.server.domain.vote.dto.response.VoteDetailResponse;
 import com.yello.server.domain.vote.dto.response.VoteFriendResponse;
 import com.yello.server.domain.vote.dto.response.VoteResponse;
+import com.yello.server.domain.vote.entity.Vote;
 import com.yello.server.domain.vote.entity.VoteRepository;
+import com.yello.server.domain.vote.exception.VoteException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -27,11 +31,20 @@ public class VoteServiceImpl implements VoteService {
 
     @Override
     public VoteDetailResponse findVoteById(Long id) {
-        return null;
+        Vote vote = findVote(id);
+        return VoteDetailResponse.of(vote);
     }
 
     @Override
     public List<VoteFriendResponse> findAllFriendVotes(Pageable pageable) {
         return null;
+    }
+
+    private Vote findVote(Long id) {
+        return voteRepository.findById(id)
+            .orElseThrow(() -> new VoteException(
+                NOT_FOUND_VOTE_EXCEPTION,
+                NOT_FOUND_VOTE_EXCEPTION.getMessage())
+            );
     }
 }
