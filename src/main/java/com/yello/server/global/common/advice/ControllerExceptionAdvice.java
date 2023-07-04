05815f32b2630bcf2db74806ab4eeac81a121a7b
hyeonejeongs <py0429@ewhain.net>
2023-07-05 00:27:26 +0900

    YEL-33 [fix] error 처리 주석 제거

--- src/main/java/com/yello/server/global/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/yello/server/global/common/advice/ControllerExceptionAdvice.java
@@ -15,6 +15,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
 
+    /**
+     * 400 BAD_REQUEST
+     */
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected BaseResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+        return BaseResponse.error(ErrorCode.REQUEST_VALIDATION_EXCEPTION,
+            String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
+    }
 
+/*
+
+    /**
+     * 500 Internal Server
+     */
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    protected BaseResponse<Object> handleException(final Exception e) {
+        return BaseResponse.error(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Sopt custom error - soptExcpetion 받아서 에러처리해줌 (어떤 에러때문인지 알 수 있음)
+     */
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<BaseResponse> handleSoptException(CustomException e) {
+        return ResponseEntity.status(e.getHttpStatus())
+            .body(BaseResponse.error(e.getError(), e.getMessage()));
+    }
 }
