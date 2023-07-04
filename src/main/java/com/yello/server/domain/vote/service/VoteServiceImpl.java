a100e38bd66098dcefcd1fc312d62438bb065f3b
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-04 15:03:43 +0900

    YEL-22 [update] readonly 트랜잭션 처리

--- src/main/java/com/yello/server/domain/vote/service/VoteServiceImpl.java
+++ src/main/java/com/yello/server/domain/vote/service/VoteServiceImpl.java
@@ -12,9 +12,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class VoteServiceImpl implements VoteService {
 
     //    private final UserRepository userRepository;
