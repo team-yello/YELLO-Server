03eab470af226a7945fc39127bab360f5a304a79
euije <exitfill1234@naver.com>
2023-07-09 10:45:36 +0900

    YEL-24 [feat] 인가 설정 추가

--- src/main/java/com/yello/server/global/configuration/SecurityConfiguration.java
+++ src/main/java/com/yello/server/global/configuration/SecurityConfiguration.java
@@ -1,20 +1,34 @@
 package com.yello.server.global.configuration;
 
+import com.yello.server.domain.user.util.AuthTokenProvider;
+import com.yello.server.global.security.AuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final AuthTokenProvider authTokenProvider;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().permitAll()
-                .and().build();
+                    .authorizeHttpRequests()
+                    .antMatchers("/auth/oauth/**").permitAll()
+                    .antMatchers("/auth/univ/**").permitAll()
+                    .antMatchers("/auth/valid").permitAll()
+                    .antMatchers("/*.html").permitAll()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/data").permitAll()
+                .and()
+                    .addFilterBefore(new AuthenticationFilter(authTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
-
 }
