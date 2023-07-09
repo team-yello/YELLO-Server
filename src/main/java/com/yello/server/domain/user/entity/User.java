755bc89bb6fca7fb7aa117447168e3ea6a0844cc
euije <exitfill1234@naver.com>
2023-07-09 11:09:43 +0900

    YEL-24 [fix] import 구문 수정

--- src/main/java/com/yello/server/domain/user/entity/User.java
+++ src/main/java/com/yello/server/domain/user/entity/User.java
@@ -3,16 +3,7 @@ package com.yello.server.domain.user.entity;
 import com.yello.server.domain.group.entity.School;
 import com.yello.server.global.common.dto.AuditingTimeEntity;
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 
 import lombok.AccessLevel;
