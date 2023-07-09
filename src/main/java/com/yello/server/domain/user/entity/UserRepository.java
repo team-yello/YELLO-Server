d154bdaf9c44e0384337251464d748aaa29cec83
euije <exitfill1234@naver.com>
2023-07-09 10:45:19 +0900

    YEL-24 [feat] User repository 함수 추가

--- src/main/java/com/yello/server/domain/user/entity/UserRepository.java
+++ src/main/java/com/yello/server/domain/user/entity/UserRepository.java
@@ -2,6 +2,17 @@ package com.yello.server.domain.user.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    // CREATE
+    User save(User user);
+
+    // READ
+    Optional<User> findByUuid(String uuid);
+    Optional<User> findAllByName(String name);
+
+    // UPDATE
+
+    // DELETE
 }
