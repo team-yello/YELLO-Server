2319aeed0c353be0cb3c1796260b450063c77478
euije <exitfill1234@naver.com>
2023-07-09 11:51:06 +0900

    YEL-26 [feat] 400 에러 추가 수정

--- src/main/java/com/yello/server/global/common/ErrorCode.java
+++ src/main/java/com/yello/server/global/common/ErrorCode.java
@@ -13,12 +13,13 @@ public enum ErrorCode {
     /**
      * 400 BAD REQUEST
      */
-    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
+    REQUEST_VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    NON_REQUIRED_REQUEST_DATA_EXCEPTION(HttpStatus.BAD_REQUEST, "요청에 필요한 값이 없습니다."),
 
     /**
      * 404 NOT FOUND
      */
-    NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다"),
+    NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
     TOKEN_TIME_EXPIRED_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 유저입니다."),
     INVALID_PASSWORD_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효하지 않은 비밀번호입니다."),
     NOT_FOUND_SAVE_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "이미지가 저장되지 않았습니다."),
@@ -27,12 +28,12 @@ public enum ErrorCode {
     /**
      * 409 CONFLICT
      */
-    ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "이미 존재하는 유저입니다"),
+    ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "이미 존재하는 유저입니다."),
 
     /**
      * 500 INTERNAL SERVER ERROR
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버 에러가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
