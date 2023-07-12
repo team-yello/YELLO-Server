e3f8407497303318cd58e7d5d934f2c525130527
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-12 19:23:17 +0900

    YEL-15 [setting] docker-compose yml 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,6 @@
 cd /home/ec2-user/app
 DOCKER_APP_NAME=yello
 
-# Redis 서버 체크
-#EXIST_REDIS=$(sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml ps | grep Up)
-#
-## Redis down 상태면 Redis up
-#if [ -z "$EXIST_REDIS" ]; then
-#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 작동중인 Redis가 존재하지 않습니다." >> /home/ec2-user/deploy.log
-#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis 이미지를 빌드합니다." >> /home/ec2-user/deploy.log
-#  sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml up -d --build
-#  sleep 10
-#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis가 작동됩니다." >> /home/ec2-user/deploy.log
-#fi
 
 # BLUE 서버 체크
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
@@ -24,7 +13,6 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Blue 배포를 시작합니다." >> /home/ec2-user/deploy.log
 
   sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-  sudo docker-compose -p yello-blue -f docker-compose.blue.yml up -d --build
   sleep 30
 
   BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
