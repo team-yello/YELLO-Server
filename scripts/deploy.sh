370c31a06bede4a4e652e023464b9947d61fac0f
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-12 16:32:38 +0900

    YEL-15 [setting] script.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,25 @@
 cd /home/ec2-user/app
 DOCKER_APP_NAME=yello
 
+# Redis 서버 체크
+EXIST_REDIS=$(sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml ps | grep Up)
+
+# Redis down 상태면 Redis up
+if [ -z "$EXIST_REDIS" ]; then
+  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 작동중인 Redis가 존재하지 않습니다." >> /home/ec2-user/deploy.log
+  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis 이미지를 빌드합니다." >> /home/ec2-user/deploy.log
+  sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml up -d
+  sleep 10
+  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis가 작동됩니다." >> /home/ec2-user/deploy.log
+fi
+
 # BLUE 서버 체크
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
-echo "[배포 시작] $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 배포를 시작합니다." >> /home/ec2-user/deploy.log
 
 # GREEN이 실행중이면 BLUE up
 if [ -z "$EXIST_BLUE" ]; then
-  echo "[BLUE] 배포가 시작됩니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Blue 배포를 시작합니다." >> /home/ec2-user/deploy.log
 
   sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
   sleep 30
@@ -18,16 +30,15 @@ if [ -z "$EXIST_BLUE" ]; then
   if [ -z "$BLUE_HEALTH" ]; then
     sudo ./slack_blue.sh
   else
-    echo "[GREEN] 서버를 종료합니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Green 서버를 종료합니다." >> /home/ec2-user/deploy.log
     sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
     sudo docker image prune -af
-    echo "[GREEN] 서버를 종료했습니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Green 서버를 종료했습니다." >> /home/ec2-user/deploy.log
   fi
 
 # BLUE가 실행중이면 GREEN up
 else
-  echo "[GREEN] 배포가 시작됩니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-
+  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Green 배포가 시작됩니다." >> /home/ec2-user/deploy.log
   sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
   sleep 30
 
@@ -36,13 +47,13 @@ else
   if [ -z "$GREEN_HEALTH" ]; then
       sudo ./slack_green.sh
   else
-    echo "[BLUE] 서버를 종료합니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Blue 서버를 종료합니다." >> /home/ec2-user/deploy.log
     sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
     sudo docker image prune -af
-    echo "[BLUE] 서버를 종료했습니다. $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+    echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Blue 서버를 종료했습니다." >> /home/ec2-user/deploy.log
   fi
 fi
 
-echo "[배포 종료] $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-echo "===================== 배포 완료 =====================" >> /home/ec2-user/deploy.log
+echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 배포를 종료합니다." >> /home/ec2-user/deploy.log
+echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 배포 프로세스 완료 =====================" >> /home/ec2-user/deploy.log
 echo >> /home/ec2-user/deploy.log
