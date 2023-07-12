b1b940ae2cc13b49528329ba3a0fca0481228fc2
euije <exitfill1234@naver.com>
2023-07-13 06:06:49 +0900

    YEL-28 [upgrade] 오류 상세 추가

---
+++ src/main/java/com/yello/server/domain/user/exception/UserConflictException.java
@@ -0,0 +1,12 @@
+package com.yello.server.domain.user.exception;
+
+import com.yello.server.global.common.ErrorCode;
+import com.yello.server.global.exception.CustomException;
+import lombok.Getter;
+
+@Getter
+public class UserConflictException extends CustomException {
+    public UserConflictException(ErrorCode error) {
+        super(error, "[UserConflictException] " + error.getMessage());
+    }
+}
