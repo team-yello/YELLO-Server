ce4e5a0bc09cddedf113316c677b9c240b294eb0
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-12 18:33:20 +0900

    YEL-15 [setting] deploy script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,16 +4,16 @@ cd /home/ec2-user/app
 DOCKER_APP_NAME=yello
 
 # Redis 서버 체크
-EXIST_REDIS=$(sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml ps | grep Up)
-
-# Redis down 상태면 Redis up
-if [ -z "$EXIST_REDIS" ]; then
-  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 작동중인 Redis가 존재하지 않습니다." >> /home/ec2-user/deploy.log
-  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis 이미지를 빌드합니다." >> /home/ec2-user/deploy.log
-  sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml up -d --build
-  sleep 10
-  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis가 작동됩니다." >> /home/ec2-user/deploy.log
-fi
+#EXIST_REDIS=$(sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml ps | grep Up)
+#
+## Redis down 상태면 Redis up
+#if [ -z "$EXIST_REDIS" ]; then
+#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] 작동중인 Redis가 존재하지 않습니다." >> /home/ec2-user/deploy.log
+#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis 이미지를 빌드합니다." >> /home/ec2-user/deploy.log
+#  sudo docker-compose -p ${DOCKER_APP_NAME}-redis -f docker-redis.yml up -d --build
+#  sleep 10
+#  echo "[$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)] Redis가 작동됩니다." >> /home/ec2-user/deploy.log
+#fi
 
 # BLUE 서버 체크
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
