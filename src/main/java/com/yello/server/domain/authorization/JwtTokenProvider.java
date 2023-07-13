a58ea6c6efd096ca192b8c2edf0569bd9d7907a0
euije <exitfill1234@naver.com>
2023-07-13 22:16:38 +0900

    YEL-28 [fix] 404 고침

--- src/main/java/com/yello/server/domain/authorization/JwtTokenProvider.java
+++ src/main/java/com/yello/server/domain/authorization/JwtTokenProvider.java
@@ -2,18 +2,14 @@ package com.yello.server.domain.authorization;
 
 import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 import static java.time.Duration.ofDays;
-import static java.time.Duration.ofHours;
 
 import com.yello.server.domain.authorization.dto.ServiceTokenVO;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -22,7 +18,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private static final Long accessTokenValidTime = ofHours(4).toMillis();
+    // 릴리즈 때 고치세요 ₩~~ >_*
+//    private static final Long accessTokenValidTime = ofHours(4).toMillis();
+    private static final Long accessTokenValidTime = ofDays(1).toMillis();
     private static final Long refreshTokenValidTime = ofDays(14).toMillis();
 
     @Value("${spring.jwt.secret}")
@@ -46,7 +44,7 @@ public class JwtTokenProvider {
     }
 
     public boolean isRefreshToken(String token, String secretKey) {
-        Header header = Jwts.parser()
+        val header = Jwts.parser()
             .setSigningKey(secretKey)
             .parseClaimsJws(token)
             .getHeader();
@@ -54,12 +52,13 @@ public class JwtTokenProvider {
         if ("refreshToken".equals(header.get("type").toString())) {
             return true;
         }
+
         return false;
     }
 
     // access 토큰 확인
     public boolean isAccessToken(String token, String secretKey) {
-        Header header = Jwts.parser()
+        val header = Jwts.parser()
             .setSigningKey(secretKey)
             .parseClaimsJws(token)
             .getHeader();
@@ -67,6 +66,7 @@ public class JwtTokenProvider {
         if ("accessToken".equals(header.get("type").toString())) {
             return true;
         }
+
         return false;
     }
 
@@ -89,18 +89,16 @@ public class JwtTokenProvider {
     }
 
     public String createJwt(Long userId, String uuid, Long tokenValidTime, String tokenType) {
-        Map<String, Object> headers = new HashMap<>();
-
         Claims claims = Jwts.claims()
             .setSubject(uuid)
             .setId(String.valueOf(userId));
 
         return Jwts.builder()
-            .setClaims(claims)
-            .setHeader(headers)
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + tokenValidTime))
-            .signWith(HS256, secretKey)
-            .compact();
+                .setClaims(claims)
+                .setHeaderParam("type", tokenType)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + tokenValidTime))
+                .signWith(HS256, secretKey)
+                .compact();
     }
 }
