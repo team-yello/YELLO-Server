133cb4b3a6dd562a22d6eb0ea5bb79c3a2188b37
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-14 12:33:31 +0900

    YEL-20 [chore] Exception 오타 수정

--- src/main/java/com/yello/server/global/common/SuccessCode.java
+++ src/main/java/com/yello/server/global/common/SuccessCode.java
@@ -20,6 +20,7 @@ public enum SuccessCode {
     CHECK_KEYWORD_SUCCESS(HttpStatus.OK, "해당 키워드 확인하는데 성공했습니다."),
     READ_YELLO_VOTE_SUCCESS(HttpStatus.OK, "옐로 투표 리스트 조회에 성공했습니다."),
     READ_YELLO_START_SUCCESS(HttpStatus.OK, "옐로 시작하기에 성공했습니다."),
+    DELETE_USER_SUCCESS(HttpStatus.OK, "유저 탈퇴에 성공했습니다."),
 
     /**
      * 201 CREATED
