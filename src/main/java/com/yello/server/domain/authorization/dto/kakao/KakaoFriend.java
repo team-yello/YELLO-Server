a56ecb2c1ebc21e9dae1d35f163108d310adc9d3
euije <exitfill1234@naver.com>
2023-07-16 01:03:17 +0900

    YEL-27 [feat] 카멜케이스 추가

--- src/main/java/com/yello/server/domain/authorization/dto/kakao/KakaoFriend.java
+++ src/main/java/com/yello/server/domain/authorization/dto/kakao/KakaoFriend.java
@@ -1,10 +1,14 @@
 package com.yello.server.domain.authorization.dto.kakao;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoFriend(
         Long id,
-        String profile_nickname,
-        String profile_thumbnail_image,
+        String profileNickname,
+        String profileThumbnailImage,
         String uuid,
         Boolean favorite,
-        Boolean allowed_msg
+        Boolean allowedMsg
 ) { }
--- src/main/java/com/yello/server/domain/authorization/dto/kakao/KakaoTokenInfo.java
+++ src/main/java/com/yello/server/domain/authorization/dto/kakao/KakaoTokenInfo.java
@@ -1,8 +1,12 @@
 package com.yello.server.domain.authorization.dto.kakao;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoTokenInfo(
     Long id,
-    Integer expires_in
+    Integer expiresIn
 ) {
 
 }
