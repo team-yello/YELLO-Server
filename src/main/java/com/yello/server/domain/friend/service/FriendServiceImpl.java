2215912576e4a0e10c14b9ebd19e70bbc2407a9c
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-18 00:27:28 +0900

    YEL-45 [hotfix] 친구 전체 조회 서비스 로직 수정

--- src/main/java/com/yello/server/domain/friend/service/FriendServiceImpl.java
+++ src/main/java/com/yello/server/domain/friend/service/FriendServiceImpl.java
@@ -1,5 +1,10 @@
 package com.yello.server.domain.friend.service;
 
+import static com.yello.server.global.common.ErrorCode.EXIST_FRIEND_EXCEPTION;
+import static com.yello.server.global.common.ErrorCode.LACK_USER_EXCEPTION;
+import static com.yello.server.global.common.ErrorCode.USERID_NOT_FOUND_USER_EXCEPTION;
+import static com.yello.server.global.common.util.ConstantUtil.RANDOM_COUNT;
+
 import com.yello.server.domain.friend.dto.FriendsResponse;
 import com.yello.server.domain.friend.dto.request.KakaoRecommendRequest;
 import com.yello.server.domain.friend.dto.response.FriendShuffleResponse;
@@ -12,19 +17,15 @@ import com.yello.server.domain.user.entity.User;
 import com.yello.server.domain.user.entity.UserRepository;
 import com.yello.server.domain.user.exception.UserException;
 import com.yello.server.domain.vote.entity.VoteRepository;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
-import static com.yello.server.global.common.ErrorCode.*;
-import static com.yello.server.global.common.util.ConstantUtil.RANDOM_COUNT;
 
 @Service
 @RequiredArgsConstructor
@@ -39,13 +40,13 @@ public class FriendServiceImpl implements FriendService {
     public FriendsResponse findAllFriends(Pageable pageable, Long userId) {
         Page<Friend> friendsData = friendRepository.findAllFriendsByUserId(pageable, userId);
         List<UserResponse> friends = friendsData.stream()
-                .map(friend -> {
-                    User user = friend.getUser();
-                    Integer friendCount = friendRepository.findAllByUser(user).size();
-                    Integer yelloCount = voteRepository.getCountAllByReceiverUserId(user.getId());
-                    return UserResponse.of(user, friendCount, yelloCount);
-                })
-                .toList();
+            .map(friend -> {
+                User user = friend.getTarget();
+                Integer friendCount = friendRepository.findAllByUser(user).size();
+                Integer yelloCount = voteRepository.getCountAllByReceiverUserId(user.getId());
+                return UserResponse.of(user, friendCount, yelloCount);
+            })
+            .toList();
 
         return FriendsResponse.of(friendsData.getTotalElements(), friends);
     }
@@ -58,7 +59,7 @@ public class FriendServiceImpl implements FriendService {
 
         Friend friendData = friendRepository.findByFollowingAndFollower(userId, targetId);
 
-        if (friendData != null) {
+        if (friendData!=null) {
             throw new FriendException(EXIST_FRIEND_EXCEPTION);
         }
 
@@ -79,9 +80,9 @@ public class FriendServiceImpl implements FriendService {
         Collections.shuffle(allFriends);
 
         return allFriends.stream()
-                .map(FriendShuffleResponse::of)
-                .limit(RANDOM_COUNT)
-                .toList();
+            .map(FriendShuffleResponse::of)
+            .limit(RANDOM_COUNT)
+            .toList();
     }
 
     @Override
@@ -89,16 +90,16 @@ public class FriendServiceImpl implements FriendService {
         User user = findUser(userId);
 
         List<User> recommendFriends = userRepository.findAllByGroupId(user.getGroup().getId(), pageable)
-                .stream()
-                .filter(recommend -> !user.getId().equals(recommend.getId()))
-                .filter(friend -> {
-                    return findFriend(userId, friend.getId()) == null;
-                })
-                .toList();
+            .stream()
+            .filter(recommend -> !user.getId().equals(recommend.getId()))
+            .filter(friend -> {
+                return findFriend(userId, friend.getId())==null;
+            })
+            .toList();
 
         return recommendFriends.stream()
-                .map(RecommendFriendResponse::of)
-                .toList();
+            .map(RecommendFriendResponse::of)
+            .toList();
     }
 
     @Transactional
@@ -112,26 +113,28 @@ public class FriendServiceImpl implements FriendService {
     }
 
     @Override
-    public List<RecommendFriendResponse> findAllRecommendKakaoFriends(Pageable pageable, Long userId, KakaoRecommendRequest request) {
+    public List<RecommendFriendResponse> findAllRecommendKakaoFriends(Pageable pageable, Long userId,
+        KakaoRecommendRequest request) {
         userRepository.findById(userId)
-                .orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION));
+            .orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION));
 
         List<String> uuidList = Arrays.stream(request.friendKakaoId())
-                .filter(friend -> {
-                    Optional<User> userByUuid = userRepository.findByUuid(friend);
-                    return friendRepository.findByFollowingAndFollower(userId, userByUuid.orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION)).getId()) == null;
-                })
-                .toList();
+            .filter(friend -> {
+                Optional<User> userByUuid = userRepository.findByUuid(friend);
+                return friendRepository.findByFollowingAndFollower(userId,
+                    userByUuid.orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION)).getId())==null;
+            })
+            .toList();
 
         return userRepository.findAllByUuidIn(uuidList, pageable).stream()
-                .map(RecommendFriendResponse::of)
-                .toList();
+            .map(RecommendFriendResponse::of)
+            .toList();
     }
 
 
     private User findUser(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION));
+            .orElseThrow(() -> new UserException(USERID_NOT_FOUND_USER_EXCEPTION));
     }
 
     private Friend findFriend(Long userId, Long friendId) {
