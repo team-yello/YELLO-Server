c0c5be9adf8dffbb3486becf4556ac263bf7d6a9
euije <exitfill1234@naver.com>
2023-07-20 02:20:56 +0900

    YEL-49 [feat] exception 정의

---
+++ src/main/java/com/yello/server/domain/authorization/exception/NotExpiredTokenForbiddenException.java
@@ -0,0 +1,12 @@
+package com.yello.server.domain.authorization.exception;
+
+import com.yello.server.global.common.ErrorCode;
+import com.yello.server.global.exception.CustomException;
+import lombok.Getter;
+
+@Getter
+public class NotExpiredTokenForbiddenException extends CustomException {
+  public NotExpiredTokenForbiddenException(ErrorCode error) {
+    super(error, "[NotExpiredTokenException] " + error.getMessage());
+  }
+}
