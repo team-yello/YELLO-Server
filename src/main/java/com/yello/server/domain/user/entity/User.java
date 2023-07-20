dc89035a63331c8ee14e977564a65593b577a39e
Kwon Sehoon <devkwonsehoon@naver.com>
2023-07-20 20:03:49 +0900

    YEL-45 [fix] User 엔티티 Where 어노테이션 추가

--- src/main/java/com/yello/server/domain/user/entity/User.java
+++ src/main/java/com/yello/server/domain/user/entity/User.java
@@ -20,12 +20,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Where(clause = "deleted_at IS NULL")
 public class User extends AuditingTimeEntity {
 
     @Id
