75fe642b8765361f02b33f5079f31cfccc02a6ca
이의제 <j@changtalk.kr>
2023-08-04 15:23:35 +0900

    YEL-78 [test] token repository 추상화

---
+++ src/main/java/com/yello/server/infrastructure/redis/repository/TokenRepository.java
@@ -0,0 +1,9 @@
+package com.yello.server.infrastructure.redis.repository;
+
+import com.yello.server.domain.authorization.dto.ServiceTokenVO;
+
+public interface TokenRepository {
+  void set(Long key, ServiceTokenVO value);
+
+  ServiceTokenVO get(Long key);
+}
