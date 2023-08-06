05b1909190138a602384d41cd5a8b29ea1118f0f
Kwon Sehoon <devkwonsehoon@naver.com>
2023-08-07 01:20:03 +0900

    YEL-88 [feat] 서비스 상호의존을 방지하고자 응답 객체 수정

--- src/main/java/com/yello/server/domain/vote/dto/response/VoteCreateResponse.java
+++ src/main/java/com/yello/server/domain/vote/dto/response/VoteCreateResponse.java
@@ -1,13 +1,23 @@
 package com.yello.server.domain.vote.dto.response;
 
+import com.yello.server.domain.vote.entity.Vote;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record VoteCreateResponse(
-    Integer point
+    Integer point,
+    List<Vote> votes
 ) {
 
-    public static VoteCreateResponse of(Integer point) {
+    public static VoteCreateResponse of(Integer point, List<Vote> votes) {
+        return VoteCreateResponse.builder()
+            .point(point)
+            .votes(votes)
+            .build();
+    }
+
+    public VoteCreateResponse toOnlyPoint() {
         return VoteCreateResponse.builder()
             .point(point)
             .build();
