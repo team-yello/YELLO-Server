d5cbd7f20aaaea0274dac22675ef0e1d13f7b78b
Kwon Sehoon <devkwonsehoon@naver.com>
2023-08-10 00:16:49 +0900

    YEL-88 [feat] 푸시 알림 구별을 위한 타입 추가

--- src/main/java/com/yello/server/infrastructure/firebase/dto/request/NotificationMessage.java
+++ src/main/java/com/yello/server/infrastructure/firebase/dto/request/NotificationMessage.java
@@ -3,19 +3,22 @@ package com.yello.server.infrastructure.firebase.dto.request;
 import com.google.firebase.messaging.Notification;
 import com.yello.server.domain.user.entity.Gender;
 import com.yello.server.domain.user.entity.User;
+import com.yello.server.infrastructure.firebase.dto.NotificationType;
 import java.text.MessageFormat;
 import lombok.Builder;
 
 @Builder
 public record NotificationMessage(
     String title,
-    String message
+    String message,
+    NotificationType type
 ) {
 
     public static NotificationMessage toVoteAvailableNotificationContent() {
         return NotificationMessage.builder()
             .title("친구에게 쪽지 보내고 포인트 받기")
             .message("대기시간이 다 지났어요. 친구들에게 투표해봐요!")
+            .type(NotificationType.VOTE_AVAILABLE)
             .build();
     }
 
@@ -23,6 +26,7 @@ public record NotificationMessage(
         return NotificationMessage.builder()
             .title(MessageFormat.format("{0}님이 회원님과 친구가 되었어요", user.getName()))
             .message("친구와 쪽지를 주고받아 보세요!")
+            .type(NotificationType.NEW_FRIEND)
             .build();
     }
 
@@ -31,6 +35,7 @@ public record NotificationMessage(
         return NotificationMessage.builder()
             .title(MessageFormat.format("{0}이 쪽지를 보냈어요!", target))
             .message("나는 너가 ...")
+            .type(NotificationType.NEW_VOTE)
             .build();
     }
 
--- src/test/java/com/yello/server/small/global/firebase/FakeFcmManger.java
+++ src/test/java/com/yello/server/small/global/firebase/FakeFcmManger.java
@@ -1,7 +1,7 @@
 package com.yello.server.small.global.firebase;
 
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
+import com.yello.server.infrastructure.firebase.dto.request.NotificationMessage;
 import com.yello.server.infrastructure.firebase.manager.FCMManager;
 
 public class FakeFcmManger implements FCMManager {
@@ -12,17 +12,20 @@ public class FakeFcmManger implements FCMManager {
     }
 
     @Override
-    public Message createMessage(String deviceToken, Notification notification) {
+    public Message createMessage(String deviceToken, NotificationMessage notificationMessage) {
         return Message.builder()
             .setToken(deviceToken)
-            .setNotification(notification)
+            .setNotification(notificationMessage.toNotification())
+            .putData("type", notificationMessage.type().name())
             .build();
     }
 
-    public Message createMessage(String deviceToken, Notification notification, String path) {
+    @Override
+    public Message createMessage(String deviceToken, NotificationMessage notificationMessage, String path) {
         return Message.builder()
             .setToken(deviceToken)
-            .setNotification(notification)
+            .setNotification(notificationMessage.toNotification())
+            .putData("type", notificationMessage.type().name())
             .putData("path", path)
             .build();
     }
