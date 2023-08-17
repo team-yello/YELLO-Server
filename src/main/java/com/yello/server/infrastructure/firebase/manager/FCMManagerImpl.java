894d97eb116d50b27e68474e237a277775b35ca7
Kwon Sehoon <devkwonsehoon@naver.com>
2023-08-17 19:57:05 +0900

    YEL-88 [update] Android silent 푸시 알림 옵션 추가

--- src/main/java/com/yello/server/infrastructure/firebase/manager/FCMManagerImpl.java
+++ src/main/java/com/yello/server/infrastructure/firebase/manager/FCMManagerImpl.java
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.ApnsConfig;
 import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.yello.server.domain.vote.repository.VoteRepository;
 import com.yello.server.infrastructure.firebase.dto.request.NotificationMessage;
 import lombok.RequiredArgsConstructor;
@@ -34,9 +35,11 @@ public class FCMManagerImpl implements FCMManager {
     public Message createMessage(String deviceToken, NotificationMessage notificationMessage, String path) {
         final Integer unreadVoteCounts = voteRepository.countUnreadByReceiverDeviceToken(deviceToken);
 
+        Notification notification = notificationMessage.toNotification();
+
         return Message.builder()
             .setToken(deviceToken)
-            .setNotification(notificationMessage.toNotification())
+            .setNotification(notification)
             .setApnsConfig(
                 ApnsConfig.builder()
                     .setAps(
@@ -47,6 +50,8 @@ public class FCMManagerImpl implements FCMManager {
                     )
                     .build()
             )
+            .putData("title", notificationMessage.title())
+            .putData("body", notificationMessage.message())
             .putData("type", notificationMessage.type().name())
             .putData("path", path)
             .putData("badge", String.valueOf(unreadVoteCounts))
