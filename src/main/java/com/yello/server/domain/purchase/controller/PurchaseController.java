f065bde954be90d98a2656d63b94052913ea84eb
이의제 <j@changtalk.kr>
2023-08-19 02:44:41 +0900

    YEL-127 [feat] 구글 인앱 결제 검증 api 구현

--- src/main/java/com/yello/server/domain/purchase/controller/PurchaseController.java
+++ src/main/java/com/yello/server/domain/purchase/controller/PurchaseController.java
@@ -1,5 +1,6 @@
 package com.yello.server.domain.purchase.controller;
 
+import static com.yello.server.global.common.SuccessCode.GOOGLE_PURCHASE_INAPP_VERIFY_SUCCESS;
 import static com.yello.server.global.common.SuccessCode.GOOGLE_PURCHASE_SUBSCRIPTION_VERIFY_SUCCESS;
 import static com.yello.server.global.common.SuccessCode.USER_PURCHASE_INFO_READ_SUCCESS;
 import static com.yello.server.global.common.SuccessCode.USER_SUBSCRIBE_NEEDED_READ_SUCCESS;
@@ -8,7 +9,9 @@ import static com.yello.server.global.common.SuccessCode.VERIFY_RECEIPT_SUCCESS;
 import com.yello.server.domain.purchase.dto.apple.AppleOrderResponse;
 import com.yello.server.domain.purchase.dto.apple.AppleTransaction;
 import com.yello.server.domain.purchase.dto.request.AppleInAppRefundRequest;
+import com.yello.server.domain.purchase.dto.request.GoogleInAppGetRequest;
 import com.yello.server.domain.purchase.dto.request.GoogleSubscriptionV2GetRequest;
+import com.yello.server.domain.purchase.dto.response.GoogleInAppV1GetResponse;
 import com.yello.server.domain.purchase.dto.response.GoogleSubscriptionV2GetResponse;
 import com.yello.server.domain.purchase.dto.response.UserPurchaseInfoResponse;
 import com.yello.server.domain.purchase.dto.response.UserSubscribeNeededResponse;
@@ -16,6 +19,7 @@ import com.yello.server.domain.purchase.service.PurchaseService;
 import com.yello.server.domain.user.entity.User;
 import com.yello.server.global.common.annotation.AccessTokenUser;
 import com.yello.server.global.common.dto.BaseResponse;
+import com.yello.server.global.common.dto.response.GoogleInAppGetResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -72,6 +76,12 @@ public class PurchaseController {
         return BaseResponse.success(VERIFY_RECEIPT_SUCCESS);
     }
 
+    @Operation(summary = "구글 구독권 결제 정보 검증하기 API", responses = {
+        @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = GoogleSubscriptionV2GetResponse.class))
+        )
+    })
     @PostMapping("/google/subscriptionsv2/verify")
     public BaseResponse<GoogleSubscriptionV2GetResponse> verifyGoogleSubscriptionTransaction(
         @AccessTokenUser User user,
@@ -81,10 +91,26 @@ public class PurchaseController {
         return BaseResponse.success(GOOGLE_PURCHASE_SUBSCRIPTION_VERIFY_SUCCESS, data);
     }
 
+    @Operation(summary = "구글 열람권 결제 정보 검증하기 API", responses = {
+        @ApiResponse(
+            responseCode = "201",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = GoogleInAppGetResponse.class))
+        )
+    })
+    @PostMapping("/google/inapp/verify")
+    public BaseResponse<GoogleInAppV1GetResponse> verifyGoogleInAppTransaction(
+        @AccessTokenUser User user,
+        @RequestBody GoogleInAppGetRequest request
+    ) throws IOException {
+        val data = purchaseService.verifyGoogleInAppTransaction(user.getId(), request);
+        return BaseResponse.success(GOOGLE_PURCHASE_INAPP_VERIFY_SUCCESS, data);
+    }
+
+
     @Operation(summary = "구독 연장 유도 필요 여부 확인 API", responses = {
         @ApiResponse(
             responseCode = "200",
-            content = @Content(mediaType = "application/json")
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserSubscribeNeededResponse.class))
         )
     })
     @GetMapping("/subscribeNeed")
