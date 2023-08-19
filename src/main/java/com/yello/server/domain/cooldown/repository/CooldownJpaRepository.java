7d33f1e9fd193231971378687d488befbbbc06fb
이의제 <j@changtalk.kr>
2023-08-19 19:09:08 +0900

    main [main] @param 추가

--- src/main/java/com/yello/server/domain/cooldown/repository/CooldownJpaRepository.java
+++ src/main/java/com/yello/server/domain/cooldown/repository/CooldownJpaRepository.java
@@ -4,22 +4,23 @@ import com.yello.server.domain.cooldown.entity.Cooldown;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CooldownJpaRepository extends JpaRepository<Cooldown, Long> {
 
     @Query("select c from Cooldown c where c.user.id = :userId and c.user.deletedAt is null")
-    Optional<Cooldown> findByUserId(Long userId);
+    Optional<Cooldown> findByUserId(@Param("userId") Long userId);
 
     @Query("select c from Cooldown c where c.user.id = :userId")
-    Optional<Cooldown> findByUserIdNotFiltered(Long userId);
+    Optional<Cooldown> findByUserIdNotFiltered(@Param("userId") Long userId);
 
     @Query("select case when count(c) > 0 then true else false end from Cooldown c " +
         "where c.user.id = :userId " +
         "and c.deletedAt is null")
-    boolean existsByUserId(Long userId);
+    boolean existsByUserId(@Param("userId") Long userId);
 
     @Query("select case when count(c) > 0 then true else false end from Cooldown c " +
         "where c.messageId = :messageId " +
         "and c.deletedAt is null")
-    boolean existsByMessageId(String messageId);
+    boolean existsByMessageId(@Param("messageId") String messageId);
 }
