d507736a2c7a45a0b43ffb5a1a924927183ade3a
이의제 <j@changtalk.kr>
2023-08-29 11:22:47 +0900

    YEL-147 [feat] fake repository 구현

--- src/test/java/com/yello/server/domain/cooldown/FakeCooldownRepository.java
+++ src/test/java/com/yello/server/domain/cooldown/FakeCooldownRepository.java
@@ -1,10 +1,16 @@
 package com.yello.server.domain.cooldown;
 
+import static com.yello.server.global.common.ErrorCode.ID_NOT_FOUND_COOLDOWN_EXCEPTION;
+
 import com.yello.server.domain.cooldown.entity.Cooldown;
+import com.yello.server.domain.cooldown.exception.CooldownNotFoundException;
 import com.yello.server.domain.cooldown.repository.CooldownRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 public class FakeCooldownRepository implements CooldownRepository {
 
@@ -13,12 +19,12 @@ public class FakeCooldownRepository implements CooldownRepository {
 
     @Override
     public Cooldown save(Cooldown cooldown) {
-        if (cooldown.getId()!=null && cooldown.getId() > id) {
+        if (cooldown.getId() != null && cooldown.getId() > id) {
             id = cooldown.getId();
         }
 
         Cooldown newCooldown = Cooldown.builder()
-            .id(cooldown.getId()==null ? ++id : cooldown.getId())
+            .id(cooldown.getId() == null ? ++id : cooldown.getId())
             .user(cooldown.getUser())
             .createdAt(cooldown.getCreatedAt())
             .deletedAt(null)
@@ -26,6 +32,21 @@ public class FakeCooldownRepository implements CooldownRepository {
 
         data.add(newCooldown);
         return newCooldown;
+    }
+
+    @Override
+    public Cooldown getById(Long cooldownId) {
+        return data.stream()
+            .filter(cooldown -> cooldown.getId().equals(cooldownId))
+            .findFirst()
+            .orElseThrow(() -> new CooldownNotFoundException(ID_NOT_FOUND_COOLDOWN_EXCEPTION));
+    }
+
+    @Override
+    public Optional<Cooldown> findById(Long cooldownId) {
+        return data.stream()
+            .filter(cooldown -> cooldown.getId().equals(cooldownId))
+            .findFirst();
     }
 
     @Override
@@ -48,6 +69,38 @@ public class FakeCooldownRepository implements CooldownRepository {
     @Override
     public Optional<Cooldown> findByUserIdNotFiltered(Long userId) {
         return Optional.empty();
+    }
+
+    @Override
+    public Long count() {
+        return (long) data.size();
+    }
+
+    @Override
+    public Long countAllByYelloIdContaining(String yelloId) {
+        return (long) data.stream()
+            .filter(cooldown -> cooldown.getUser().getYelloId().contains(yelloId))
+            .toList()
+            .size();
+    }
+
+    @Override
+    public Page<Cooldown> findAll(Pageable pageable) {
+        final List<Cooldown> cooldownList = data.stream()
+            .skip(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .toList();
+        return new PageImpl<>(cooldownList);
+    }
+
+    @Override
+    public Page<Cooldown> findAllContaining(Pageable pageable, String yelloId) {
+        final List<Cooldown> cooldownList = data.stream()
+            .filter(cooldown -> cooldown.getUser().getYelloId().contains(yelloId))
+            .skip(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .toList();
+        return new PageImpl<>(cooldownList);
     }
 
     @Override
