cab6dec1d40fcd8c717f59812d7d8e748f380b25
이의제 <j@changtalk.kr>
2023-08-29 11:24:41 +0900

    YEL-147 [feat] user, cooldown 의존 구현

--- src/main/java/com/yello/server/domain/cooldown/repository/CooldownRepositoryImpl.java
+++ src/main/java/com/yello/server/domain/cooldown/repository/CooldownRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.yello.server.domain.cooldown.repository;
 
+import static com.yello.server.global.common.ErrorCode.ID_NOT_FOUND_COOLDOWN_EXCEPTION;
+
 import com.yello.server.domain.cooldown.entity.Cooldown;
+import com.yello.server.domain.cooldown.exception.CooldownNotFoundException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,17 @@ public class CooldownRepositoryImpl implements CooldownRepository {
     @Override
     public Cooldown save(Cooldown cooldown) {
         return cooldownJpaRepository.save(cooldown);
+    }
+
+    @Override
+    public Cooldown getById(Long cooldownId) {
+        return cooldownJpaRepository.findById(cooldownId)
+            .orElseThrow(() -> new CooldownNotFoundException(ID_NOT_FOUND_COOLDOWN_EXCEPTION));
+    }
+
+    @Override
+    public Optional<Cooldown> findById(Long cooldownId) {
+        return cooldownJpaRepository.findById(cooldownId);
     }
 
     @Override
@@ -36,6 +52,26 @@ public class CooldownRepositoryImpl implements CooldownRepository {
     @Override
     public Optional<Cooldown> findByUserIdNotFiltered(Long userId) {
         return cooldownJpaRepository.findByUserIdNotFiltered(userId);
+    }
+
+    @Override
+    public Long count() {
+        return cooldownJpaRepository.count();
+    }
+
+    @Override
+    public Long countAllByYelloIdContaining(String yelloId) {
+        return cooldownJpaRepository.countAllByUserYelloIdContaining(yelloId);
+    }
+
+    @Override
+    public Page<Cooldown> findAll(Pageable pageable) {
+        return cooldownJpaRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Cooldown> findAllContaining(Pageable pageable, String yelloId) {
+        return cooldownJpaRepository.findAllByUserYelloIdContaining(pageable, yelloId);
     }
 
     @Override
