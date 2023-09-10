280663c0a0d893c17dc56633e6f2231f0f794140
hyeonjeongs <py0429@ewhain.net>
2023-09-10 19:59:55 +0900

    YEL-154 [feat] json parse 수정

--- src/main/java/com/yello/server/domain/purchase/dto/apple/ApplePayloadDataVO.java
+++ src/main/java/com/yello/server/domain/purchase/dto/apple/ApplePayloadDataVO.java
@@ -1,8 +1,12 @@
 package com.yello.server.domain.purchase.dto.apple;
 
+import lombok.Builder;
+
+@Builder
 public record ApplePayloadDataVO(
     String appAppleId,
     String bundleId,
+    String bundleVersion,
     String environment,
     String signedTransactionInfo,
     String signedRenewalInfo,
