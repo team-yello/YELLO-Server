233eac4a71846cf5b3f1add69050103b671be8dd
user 이름 <user >
2024-01-07 17:36:17 +0900

    YEL-181 [feat] 보일러플레이트 코드 추가

--- src/main/java/com/yello/server/domain/notice/entity/Notice.java
+++ src/main/java/com/yello/server/domain/notice/entity/Notice.java
@@ -7,10 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice extends AuditingTimeEntity {
 
     @Id
--- src/main/java/com/yello/server/domain/user/entity/UserData.java
+++ src/main/java/com/yello/server/domain/user/entity/UserData.java
@@ -9,10 +9,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserData {
 
     @Id
