1aa5e7c5e011c322c4edda86174167e516c3d61a
Lee Euije <exitfill1234@naver.com>
2024-03-25 14:35:24 +0900

    staging [staging] fix typo

--- src/main/java/com/yello/server/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/yello/server/domain/user/repository/UserRepositoryImpl.java
@@ -7,11 +7,8 @@ import static com.yello.server.global.common.ErrorCode.USERID_NOT_FOUND_USER_EXC
 import static com.yello.server.global.common.ErrorCode.YELLOID_NOT_FOUND_USER_EXCEPTION;
 
 import com.querydsl.core.BooleanBuilder;
-
-import com.yello.server.domain.user.entity.Gender;
-import com.querydsl.core.QueryMetadata;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yello.server.domain.user.entity.Gender;
 import com.yello.server.domain.user.entity.User;
 import com.yello.server.domain.user.exception.UserNotFoundException;
 import java.util.List;
@@ -29,7 +26,6 @@ public class UserRepositoryImpl implements UserRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
     private final UserJpaRepository userJpaRepository;
-    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public User save(User user) {
